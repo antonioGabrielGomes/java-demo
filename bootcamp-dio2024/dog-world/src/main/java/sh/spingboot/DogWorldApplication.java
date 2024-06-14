bf029ce142df1e88f6ab2717520a11a8b8d74947
antonioGabrielGomes <antonioggca000@gmail.com>
2024-06-14 00:07:27 -0300

    bootc-trash

---
+++ bootcamp-dio2024/dog-world/src/main/java/sh/spingboot/DogWorldApplication.java
@@ -0,0 +1,15 @@
+package sh.spingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogWorldApplication.class, args);
+
+
+	}
+
+}
---
+++ bootcamp-dio2024/dog-world/src/test/java/sh/spingboot/DogWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.spingboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
